940080f49f8a22b348eaf91b8ba0c78068cb8d3f
yaxian.gu <yaxian.gu@gmail.com>
2023-08-29 23:09:12 +0800

    fix: exports is not defined in ES module scope when node >= 16

---
+++ examples/nuxt3/app.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+  import Dropzone from './components/Dropzone.vue'
+
+</script>
+
+<template>
+  <img alt="Vue logo" src="./public/logo.png" />
+  <Dropzone />
+</template>
+
+<style>
+#__nuxt {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
